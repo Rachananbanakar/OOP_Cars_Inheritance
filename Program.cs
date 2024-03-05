d35c155eb22e2e7fc9bc77aca618c187d7309d85
Rachana_Nagaraj_Banakar <rachananagarajb@usf.edu>
2024-03-05 11:52:42 -0500

    Removed comments

--- Program.cs
+++ Program.cs
@@ -7,10 +7,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Creating an instance of Car
         Car myCar = new Car("Toyota", "Corolla", 2020, "XYZ123");
 
-        // Displaying information about the car
         Console.WriteLine(myCar.DisplayInfo());
     }
 }
--- Vehicle.cs
+++ Vehicle.cs
@@ -10,23 +10,18 @@ namespace VehicleExample
         public string Model { get; set; }
         public int Year { get; set; }
 
-        // Default constructor
         public Vehicle()
         {
             Brand = "Unknown";
             Model = "Unknown";
             Year = 0;
         }
-
-        // Parametrized constructor
         public Vehicle(string brand, string model, int year)
         {
             Brand = brand;
             Model = model;
             Year = year;
         }
-
-        // Method to display basic vehicle info
         public virtual string DisplayInfo()
         {
             return $"Brand: {Brand}, Model: {Model}, Year: {Year}";
